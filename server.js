de28a3d2553fb1df04abbcccf968ca343bd1f2bc
Hadi <969hadiahmed@gmail.com>
2025-07-10 11:12:17 +0530

    feat: /hello/{name} route

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 import Hapi from "@hapi/hapi"
 
+const helloName = (request, h) => {
+  const name = request.params.name;
+  return `hello ${name}`
+}
+
 const init = async () => {
   const server = Hapi.server({
     port: 3000,
@@ -9,9 +14,12 @@ const init = async () => {
   server.route([{
     method: 'GET',
     path: '/',
-    handler: (request, h) => {
-      return 'Hello from Hapi!';
-    }
+    handler: helloName
+  },
+  {
+    method: 'GET',
+    path: '/hello/{name}',
+    handler: helloName
   }]);
 
   await server.start();
